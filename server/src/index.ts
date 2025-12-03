8e152684d6ef88871beec85b43602f6c20b4f3d8
owk-owk130 <owaki-kengo@kayac.com>
2025-12-03 18:18:52 +0900

    mastra繋ぎ込みサンプル

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,26 @@
 import { Hono } from "hono";
+import { stream } from "hono/streaming";
+import { mastra } from "./mastra";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
+});
+
+app.get("/weather", async (c) => {
+	const run = await mastra.getWorkflow("weatherWorkflow").createRun();
+
+	const workflowStream = run.stream({
+		inputData: { city: "tokyo" },
+	});
+
+	return stream(c, async (stream) => {
+		for await (const chunk of workflowStream) {
+			console.log("Sending chunk:", chunk);
+			await stream.write(JSON.stringify(chunk));
+		}
+	});
 });
 
 export default app;
