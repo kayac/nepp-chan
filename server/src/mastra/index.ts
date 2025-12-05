44ec7c3391b6f5bfe8f3cce5c2cada5f4c324817
owk-owk130 <owaki-kengo@kayac.com>
2025-12-05 10:46:58 +0900

    NOTEつけておく

--- server/src/mastra/index.ts
+++ server/src/mastra/index.ts
@@ -1,6 +1,11 @@
 import { LibSQLStore } from "@mastra/libsql";
 import { createMastra } from "~/mastra/factory";
 
+/**
+ * NOTE: mastra playground用のmastraインスタンス
+ * アプリケーション側で利用する時は、factory.tsのcreateMastra関数を利用して生成してください。
+ *  */
+
 export const mastra = createMastra(
   new LibSQLStore({
     id: "mastra-storage",
